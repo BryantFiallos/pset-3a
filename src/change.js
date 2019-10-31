a19570bd77c32d95b4254a34f64f91516d31be88
Bryant Fiallos <bryant.fiallos@gmail.com>
2019-10-31 10:52:12 -0400

    commit

--- src/change.js
+++ src/change.js
@@ -2,13 +2,15 @@ const readlineSync = require("readline-sync");
 
 const MIN = 0.01;
 const MAX = 0.99;
-
+const quarter = 0.25;
+const dime = 0.1;
+const nickel = 0.05;
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("\nInvalid.");
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("\nInvalid.");
 } else {
     // write your code for making change here
 }

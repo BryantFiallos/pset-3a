8256790d49419df248a57da35464bfe8c82ff132
unknown <bryant.fiallos@gmail.com>
2019-11-06 18:20:03 -0500

    years

---
+++ src/years.js
@@ -0,0 +1,14 @@
+const readlineSync = require("readline-sync");
+
+let year = Number(readlineSync.question("\nEnter a year: "));
+
+if (!Number.isNaN(year)) {
+  if (year>0 && year<= Number.MAX_SAFE_INTEGER) {
+    if (year%4==0) {
+      if (year%100!=0) {
+        console.log("\n" + year + " is a leap year.");
+      }
+      else {
+        if (year%400==0) {
+          console.log("\n" + year + " is a leap year.");
+        }
